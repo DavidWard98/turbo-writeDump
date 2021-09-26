8bf1564fb1dc39659953afbd98f65ad6515e987a
DavidWard98 <daeward98@gmail.com>
2021-09-26 14:39:34 -0600

    cfml language extension activiation

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('turbo-writedump.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('turbo-writedump.writeDump', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from turbo writeDump!');
+		vscode.window.showInformationMessage('WriteDump!');
 	});
 
 	context.subscriptions.push(disposable);
